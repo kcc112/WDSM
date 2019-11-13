4270c6264c2e81aa0f827c809fed4ad5e482b808
Mateusz <wasilewskimateusz1998@gmail.com>
2019-11-13 02:05:39 +0100

    Finish ScoreScreen

--- screens/ScoreScreen.js
+++ screens/ScoreScreen.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
-import { VictoryBar, VictoryStack } from "victory-native";
+import { VictoryBar, VictoryStack, VictoryChart, VictoryAxis } from "victory-native";
 import { Button } from 'react-native-elements'
 
 
 
 const ScoreScreen = props => {
 
-    const data = [
-        { quarter: 1, earnings: 13000 },
-        { quarter: 2, earnings: 16500 },
-        { quarter: 3, earnings: 14250 },
-        { quarter: 4, earnings: 19000 }
+    const dataYes = [
+        { id: 1, approvals: 3 },
+        { id: 2, approvals: 9 },
+        { id: 3, approvals: 7 },
+        { id: 4, approvals: 5 }
+    ];
+
+    const dataNo = [
+        { id: 1, approvals: 10-dataYes[0].approvals, name: "Fakultet1" },
+        { id: 2, approvals: 10-dataYes[1].approvals, name: "Fakultet2" },
+        { id: 3, approvals: 10-dataYes[2].approvals, name: "Fakultet3" },
+        { id: 4, approvals: 10-dataYes[3].approvals, name: "Fakultet4" }
     ];
 
 
@@ -31,29 +38,45 @@ const ScoreScreen = props => {
             </View>
             <Text style={styles.sumUp}>Summary</Text>
 
-            <VictoryStack
-                colorScale={["green", "red" ]}
-                animate={{
-                    duration: 2000,
-                    onLoad: { duration: 500 }
-                  }}
-                  horizontal = {true}
-                  width={400}
-                  
-                  
+            <VictoryChart
+                domainPadding={30}
+                padding={{ left: 80, right: 30 }}
             >
-                <VictoryBar
-                    data={[{ x: "1", y: 6}, { x: "2", y: 2 }]}
+                <VictoryAxis
+                dependent Axis
+                    tickValues={[1, 2, 3, 4]}
+                    tickFormat={[dataNo[3].name, dataNo[2].name, dataNo[1].name, dataNo[0].name]}
                     
                 />
-                <VictoryBar
-                    data={[{ x: "1", y: 4 }, { x: "2", y: 8 }]}
-                />
-            </VictoryStack>
+                <VictoryStack
+                    colorScale={["green", "red"]}
+                    animate={{
+                        duration: 2000,
+                        onLoad: { duration: 500 }
+                    }}
+                    horizontal={true}   
+                >
+                    <VictoryBar
+                        data={dataYes}
+                        
+                        x="id"
+                        y="approvals"
+                        
+                    />
+                       <VictoryBar
+
+                        data={dataNo}
+                        x="id"
+                        y="approvals"
+                        labels={({ datum }) => ` ${(10-datum.approvals)/10 *100} %`}
+                    />
+ 
+                </VictoryStack>
+            </VictoryChart>
 
             <View style={styles.buttonContainer}>
-                <Button buttonStyle={ styles.buttonColor } title="Retry" color='black' onPress={retryHandler}></Button>
-                <Button buttonStyle={ styles.buttonColor } title="Home" color='black' onPress={props.scoreVisiblitySetter}></Button>
+                <Button buttonStyle={styles.buttonColor} title="Retry" color='black' onPress={retryHandler}></Button>
+                <Button buttonStyle={styles.buttonColor} title="Home" color='black' onPress={props.scoreVisiblitySetter}></Button>
             </View>
         </View>
     );
@@ -73,21 +96,23 @@ const styles = StyleSheet.create({
     },
     sumUp: {
         fontSize: 25,
-        marginTop: '-10%'
+        marginTop: '-20%'
     },
 
     buttonContainer: {
+        flex: 1,
         flexDirection: "row",
         width: '70%',
         justifyContent: 'space-between',
+        alignItems: 'center',
         paddingHorizontal: 15,
-        
+
     },
     buttonColor: {
         borderRadius: 20,
         backgroundColor: '#b30c00',
         width: 100,
-      },
+    },
 
 });
 
