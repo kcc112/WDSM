ee74b7ecf332d84e761a8e481f57d120bf1cb84a
Kamil Celejewski <kamil12283@gmail.com>
2019-11-18 18:34:44 +0100

    [Fix]

--- components/Questions.js
+++ components/Questions.js
@@ -3,16 +3,16 @@ export const Questions = [
     { id: "2", uri: require("../assets/splash.png"), text: "example 2", name: "DGK" },
     { id: "3", uri: require("../assets/splash.png"), text: "example 3", name: "DGK" },
     { id: "4", uri: require("../assets/splash.png"), text: "example 4", name: "DGK" },
-    { id: "6", uri: require("../assets/splash.png"), text: "example 5", name: "IRZ" },
-    { id: "7", uri: require("../assets/splash.png"), text: "example 6", name: "IRZ" },//IRZ - internet rzeczy
-    { id: "8", uri: require("../assets/splash.png"), text: "example 7", name: "IRZ" },
-    { id: "9", uri: require("../assets/splash.png"), text: "example 8", name: "IRZ" },
-    { id: "11", uri: require("../assets/splash.png"), text: "example 9", name: "WDSM" },//WDSM - wstęp do systemów mobilnych
-    { id: "12", uri: require("../assets/splash.png"), text: "example 10", name: "WDSM" },
-    { id: "13", uri: require("../assets/splash.png"), text: "example 11", name: "WDSM" },
-    { id: "14", uri: require("../assets/splash.png"), text: "example 12", name: "WDSM" },
-    { id: "16", uri: require("../assets/splash.png"), text: "example 13", name: "WZAIP" },//WZAIP - wybrane zagadnienia algorytmiki i programowania
-    { id: "17", uri: require("../assets/splash.png"), text: "example 14", name: "WZAIP" },
-    { id: "18", uri: require("../assets/splash.png"), text: "example 15", name: "WZAIP" },
-    { id: "19", uri: require("../assets/splash.png"), text: "example 16", name: "WZAIP" },
+    { id: "5", uri: require("../assets/splash.png"), text: "example 5", name: "IRZ" },
+    { id: "6", uri: require("../assets/splash.png"), text: "example 6", name: "IRZ" },//IRZ - internet rzeczy
+    { id: "7", uri: require("../assets/splash.png"), text: "example 7", name: "IRZ" },
+    { id: "8", uri: require("../assets/splash.png"), text: "example 8", name: "IRZ" },
+    { id: "9", uri: require("../assets/splash.png"), text: "example 9", name: "WDSM" },//WDSM - wstęp do systemów mobilnych
+    { id: "10", uri: require("../assets/splash.png"), text: "example 10", name: "WDSM" },
+    { id: "11", uri: require("../assets/splash.png"), text: "example 11", name: "WDSM" },
+    { id: "12", uri: require("../assets/splash.png"), text: "example 12", name: "WDSM" },
+    { id: "13", uri: require("../assets/splash.png"), text: "example 13", name: "WZAIP" },//WZAIP - wybrane zagadnienia algorytmiki i programowania
+    { id: "14", uri: require("../assets/splash.png"), text: "example 14", name: "WZAIP" },
+    { id: "15", uri: require("../assets/splash.png"), text: "example 15", name: "WZAIP" },
+    { id: "16", uri: require("../assets/splash.png"), text: "example 16", name: "WZAIP" },
   ].sort(() => Math.random() - 0.5);
