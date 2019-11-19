2584404f4424ef0a635de0194ec3ece5ca6f54a8
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-11-19 03:06:20 +0100

    Add questions and images to them

--- components/Questions.js
+++ components/Questions.js
@@ -1,18 +1,18 @@
 export const Questions = [
-    { id: "1", uri: require("../assets/splash.png"), text: "example 1", name: "DGK" },//DGK - dwuwymiarowe gry komputerowe
-    { id: "2", uri: require("../assets/splash.png"), text: "example 2", name: "DGK" },
-    { id: "3", uri: require("../assets/splash.png"), text: "example 3", name: "DGK" },
-    { id: "4", uri: require("../assets/splash.png"), text: "example 4", name: "DGK" },
-    { id: "5", uri: require("../assets/splash.png"), text: "example 5", name: "IRZ" },
-    { id: "6", uri: require("../assets/splash.png"), text: "example 6", name: "IRZ" },//IRZ - internet rzeczy
-    { id: "7", uri: require("../assets/splash.png"), text: "example 7", name: "IRZ" },
-    { id: "8", uri: require("../assets/splash.png"), text: "example 8", name: "IRZ" },
-    { id: "9", uri: require("../assets/splash.png"), text: "example 9", name: "WDSM" },//WDSM - wstęp do systemów mobilnych
-    { id: "10", uri: require("../assets/splash.png"), text: "example 10", name: "WDSM" },
-    { id: "11", uri: require("../assets/splash.png"), text: "example 11", name: "WDSM" },
-    { id: "12", uri: require("../assets/splash.png"), text: "example 12", name: "WDSM" },
-    { id: "13", uri: require("../assets/splash.png"), text: "example 13", name: "WZAIP" },//WZAIP - wybrane zagadnienia algorytmiki i programowania
-    { id: "14", uri: require("../assets/splash.png"), text: "example 14", name: "WZAIP" },
-    { id: "15", uri: require("../assets/splash.png"), text: "example 15", name: "WZAIP" },
-    { id: "16", uri: require("../assets/splash.png"), text: "example 16", name: "WZAIP" },
+    { id: "1", uri: require("../assets/TPA_1.png"), text: "Lubisz wyzwania i programowanie obiektowe? Chesz zrozumieć język programowania od podstaw?", name: "DGK" },//DGK - dwuwymiarowe gry komputerowe -> TPA - technologie programowania adaptacyjnego
+    { id: "2", uri: require("../assets/TPA_2.png"), text: "Chcesz poznać czym są wzorce projektowe i jak wykorzystywać je w praktyce?", name: "DGK" },
+    { id: "3", uri: require("../assets/TPA_3.png"), text: "Masz dość nudnych, tradycyjnych wykładów? Pragniesz prowadzić aktywnie dyskusje z prowadzącym i wspólnie rozwiązywać problemy?", name: "DGK" },
+    { id: "4", uri: require("../assets/TPA_4.png"), text: "Chciałbyś nauczyć się efektywnie korzystać ze zintegrowanego środowiska programistycznego?", name: "DGK" },
+    { id: "5", uri: require("../assets/PBS_1.png"), text: "Chcesz zdobyć umiejętności identyfikowania podatności na ataki aplikacji i systemów IoT?", name: "IRZ" },
+    { id: "6", uri: require("../assets/PBS_2.png"), text: "Pragniesz umieć wykryć nieuprawnioną aktywność w swoim systemie?", name: "IRZ" },//IRZ - internet rzeczy
+    { id: "7", uri: require("../assets/PBS_3.png"), text: "Po ukończeniu kursu będziesz w stanie tworzyć wielowarstwowe zabezpieczenia systemów IoT, zainteresowany?", name: "IRZ" },
+    { id: "8", uri: require("../assets/PBS_4.png"), text: "Chciałbyś uzyskać jeden z certyfikatów, np. Certified Ethical Hacking (CEH)?", name: "IRZ" },
+    { id: "9", uri: require("../assets/ZZP_1.png"), text: "Chcesz znacznie pogłębić swoją wiedzę z języka Java?", name: "WDSM" },//WDSM - wstęp do systemów mobilnych
+    { id: "10", uri: require("../assets/ZZP_2.png"), text: "Chciałbyś wiedzieć jak programować obiektowo w kontekście rzeczywistych problemów biznesowych?", name: "WDSM" },
+    { id: "11", uri: require("../assets/ZZP_3.png"), text: "Będziesz umieć zarządzać jakością swojego kodu.", name: "WDSM" },
+    { id: "12", uri: require("../assets/ZZP_4.png"), text: "Będziesz w stanie tworzyć oprogramowanie na podstawie wymagań biznesowych. ", name: "WDSM" },
+    { id: "13", uri: require("../assets/WZAIP_1.png"), text: "Po zakończeniu przedmiotu będziesz w pełni rozumieć klasyczne algorytmy oraz umieć je zastosować w praktyce.", name: "WZAIP" },//WZAIP - wybrane zagadnienia algorytmiki i programowania
+    { id: "14", uri: require("../assets/WZAIP_2.png"), text: "Przedmiot przygotuje Cię do pracy pod presją czasu oraz nauczy współzawodnictwa przy tworzeniu kodu.", name: "WZAIP" },
+    { id: "15", uri: require("../assets/WZAIP_3.png"), text: "Będziesz w stanie tworzyć własne algorytmy do rozwiązywania napotkanych problemów.", name: "WZAIP" },
+    { id: "16", uri: require("../assets/WZAIP_4.png"), text: "Chciałbyś znać swoje braki w wiedzy z zakresu algorytmów oraz znać kierunek dalszego rozwoju w tej dziedzinie?", name: "WZAIP" },
   ].sort(() => Math.random() - 0.5);
